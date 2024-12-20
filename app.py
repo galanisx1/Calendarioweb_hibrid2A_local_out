d950ea21661fee8c35addd556ebc198c66367dd8
galanisx1 <100341218+galanisx1@users.noreply.github.com>
2024-12-19 20:57:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
 from tables import calendar_table, pricing_tables, pricing_tables_credit, pricing_tables_extra
-from flask_cors import CORS
-import os
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS para solicitudes desde el frontend
-
 
 @app.route("/")
 def index():
@@ -14,4 +10,6 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
